2bd7600fe4026b121985bfb527bd4e3852355b1b
SmitaDPatil <Smitadp85@gmail.com>
2024-01-17 11:38:35 +0530

    [smita]:new branch created and updated test case

---
+++ src/test/Domain.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Domain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi this is pull request");
+
+	}
+
+}
